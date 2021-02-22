179f5e4972261f00f562d8d676d35553e85b9167
Chirag Lulla <lullachirag239@gmail.coom>
2021-02-23 00:11:13 +0530

    :sparkles: animate the score

--- Quiz/app.js
+++ Quiz/app.js
@@ -3,22 +3,25 @@ const form = document.querySelector('.quiz-form')
 const resultSection = document.querySelector('.result')
 let numberOfQuestions = 4
 
-form.addEventListener('submit', (e)=>{
+form.addEventListener('submit', (e) => {
     e.preventDefault()
-    let html = ""
     let userScore = 0
     let correctCount = 0
     const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value]
-
     userAnswers.forEach((answer, index) => {
         if(answer == correctAnswers[index]){
             correctCount++
         }
     })
     userScore = (correctCount/numberOfQuestions)*100
-    html+=`<div class="container lead">
-             <p class="text-white display-4">You are <span class="text-success display-3 p-3">${userScore}%</span> OP</p>
-           </div>`
-    resultSection.innerHTML = html
+    scrollTo(0, 0)
     resultSection.classList.remove('d-none')
+    let output = 0
+    const timer = setInterval(()=>{
+        resultSection.querySelector('span').textContent =`${output}%`
+        if(output === userScore)
+            clearInterval(timer)
+        else
+            output++
+    }, 8)
 })
