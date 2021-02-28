99d793f48873e873da964161e3e9903e6debcaae
Chirag Lulla <lullachirag239@gmail.coom>
2021-03-01 00:08:15 +0530

    :sparkles: add right and wrong border color

--- Quiz/app.js
+++ Quiz/app.js
@@ -1,20 +1,34 @@
-var _0xc462=["\x43","\x41","\x42"];
+var _0xc462=["\x43","\x41","\x42"]
 const correctAnswers=[_0xc462[0],_0xc462[1],_0xc462[2],_0xc462[0]]
 const form = document.querySelector('.quiz-form')
 const resultSection = document.querySelector('.result')
+let questionBlocks = Array.from(form.children)
+questionBlocks.pop()
 let numberOfQuestions = 4
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
     let userScore = 0
-    let correctCount = 0
+    let correctIndexes = [] 
+    let wrongIndexes = []
     const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value]
+
     userAnswers.forEach((answer, index) => {
         if(answer == correctAnswers[index]){
-            correctCount++
+            correctIndexes.push(index)
+        }else{
+            wrongIndexes.push(index)
         }
     })
-    userScore = (correctCount/numberOfQuestions)*100
+
+    correctIndexes.forEach((index) => {
+        questionBlocks[index].classList.add("border", "border-success", "rounded", "p-5")
+    })
+    wrongIndexes.forEach((index) => {
+        questionBlocks[index].classList.add("border", "border-danger", "rounded", "p-5")
+    })
+
+    userScore = (correctIndexes.length/numberOfQuestions)*100
     scrollTo(0, 0)
     resultSection.classList.remove('d-none')
     let output = 0
