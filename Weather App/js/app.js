25e0ffc431650b8cfa5cfc1cb32ddf4df38ac871
Chirag Lulla <lullachirag239@gmail.coom>
2021-03-23 22:59:00 +0530

    :construction::sparkles: make API call for weather info (not completed)

--- Weather App/js/app.js
+++ Weather App/js/app.js
@@ -1,5 +1,15 @@
 const api_key = 'CqkNBjmSKFeL5Wt2TyLXPNSAlWtBwkbj'
 
+const get_weather_info = async (key) => {
+    const base_url = 'http://dataservice.accuweather.com/currentconditions/v1/'
+    const query = `${key}?apikey=${api_key}`
+
+    const response = await fetch(base_url + query)
+    const data = await response.json()
+
+    console.log(data)
+}
+
 const get_city_info = async (city_name) => {
     const base_url = 'http://dataservice.accuweather.com/locations/v1/cities/search'
     const query = `?apikey=${api_key}&q=${city_name}`;
