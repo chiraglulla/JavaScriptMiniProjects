46e1a0364e4a0d1451543a13aa4acdda2db25ce2
Chirag Lulla <lullachirag239@gmail.coom>
2021-04-26 22:57:02 +0530

    :star: create accordion, snackbar, tabbed content

---
+++ UI-library/src/components/snackbar.js
@@ -0,0 +1,22 @@
+import './styles/snackbar.css'
+
+class Snackbar {
+    constructor() {
+       this.snackbar = document.createElement('div') 
+    }
+
+    initialize() {
+        this.snackbar.classList.add('snackbar')
+        document.querySelector('body').appendChild(this.snackbar)
+    }
+
+    show(message) {
+        this.snackbar.textContent = message
+        this.snackbar.classList.add('active')
+        setTimeout(() => {
+            this.snackbar.classList.remove('active')
+        }, 4000)
+    }
+}
+
+export { Snackbar as default }
--- UI-library/src/index.js
+++ UI-library/src/index.js
@@ -1,6 +1,30 @@
 import Tooltip from './components/tooltip'
+import Dropdown from './components/dropdown'
+import Tabs from './components/tab'
+import Snackbar from './components/snackbar'
+
 
 // create a tooltip
 
 const tooltip = new Tooltip(document.querySelector('#tooltip'))
 tooltip.tooltip()
+
+
+const dropdowns = document.querySelectorAll('.dropdown')
+
+dropdowns.forEach(dropdown => {
+    const dd = new Dropdown(dropdown)
+    dd.initialize()
+})
+
+const tabs = new Tabs(document.querySelector('.tabs'))
+tabs.initialize()
+
+const snackbar = new Snackbar()
+snackbar.initialize()
+
+const button = document.querySelector('.snackbar-trigger')
+
+button.addEventListener('click', () => {
+    snackbar.show('You clicked me now!;)')
+})
