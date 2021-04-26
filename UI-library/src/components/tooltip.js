720e95db13f64b9742acb1dda71df0452c984912
Chirag Lulla <lullachirag239@gmail.coom>
2021-04-26 15:33:06 +0530

    :star: create a tooltip component

---
+++ UI-library/src/components/tooltip.js
@@ -0,0 +1,26 @@
+import './styles/tooltip.css'
+
+class Tooltip {
+    constructor(element) {
+        this.element = element
+        this.message = element.getAttribute('data-message')
+    }
+
+    tooltip() {
+        const tip = document.createElement('div')
+        tip.classList.add('tip')
+        tip.textContent = this.message
+        this.element.appendChild(tip)
+
+        this.element.addEventListener('mouseenter', () => {
+            tip.classList.add('active')
+        })
+
+        this.element.addEventListener('mouseleave', () => {
+            tip.classList.remove('active')
+        })
+
+    }
+}
+
+export { Tooltip as default }
--- UI-library/src/index.js
+++ UI-library/src/index.js
@@ -1,1 +1,6 @@
-console.log("test")
+import Tooltip from './components/tooltip'
+
+// create a tooltip
+
+const tooltip = new Tooltip(document.querySelector('#tooltip'))
+tooltip.tooltip()
